e8a093910cee2d037c6769bfa5d59a81024d4cf8
Vamos Akos <vamos.akos1337@gmail.com>
2023-12-06 21:14:51 +0100

    updated restapi function names

--- MyApplication/src/main/java/com/akos/vamos/MyApplication/RestApi.java
+++ MyApplication/src/main/java/com/akos/vamos/MyApplication/RestApi.java
@@ -6,12 +6,12 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/car")
 public class RestApi {
     @GetMapping("/{id}")
-    public Car getDataById(@PathVariable int id) {
+    public Car getCarById(@PathVariable int id) {
         return new Car(id, "ABC123", "Sedan");
     }
 
     @PostMapping
-    public void postData(@RequestBody Car car) {
+    public void postCar(@RequestBody Car car) {
         System.out.println("Received data: " + car);
     }
 }
